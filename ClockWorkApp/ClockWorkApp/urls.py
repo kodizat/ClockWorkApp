9e5f27f12972d252b5963a453e6445b1e4d39f0a
Kodi <144355274+kodizat@users.noreply.github.com>
2025-03-28 12:51:56 -0400

    syncing changes

--- ClockWorkApp/ClockWorkApp/urls.py
+++ ClockWorkApp/ClockWorkApp/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from tracker import views
 
 urlpatterns = [
+    path('register/', views.register_view, name='home'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
