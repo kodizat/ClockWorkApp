85e4b7cce8ec4647d54663622c7ff0bc1237e223
ConnerF6 <134663172+ConnerF6@users.noreply.github.com>
2025-04-28 09:53:01 -0400

    migrations

--- ClockWorkApp/ClockWorkApp/settings.py
+++ ClockWorkApp/ClockWorkApp/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tracker.migrations.middleware.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'ClockWorkApp.urls'
